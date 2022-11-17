100d1c89e85dd0d794a9d3108618eb4f4ac301c4
Ronan <spacechickenrobot@gmail.com>
2022-11-17 11:48:52 -0500

    Drive Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode.subsystem
+
+public class Drive {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public Drive(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");
+
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        frontRight.setDirection(DcMotor.Direction.FORWARD);
+        backRight.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    public void strafeLeft(double power) {
+        frontRight.setPower(power);
+        backRight.setPower(power*-1);
+        frontLeft.setPower(power*-1);
+        backLeft.setPower(power);
+    }
+
+    public void strafeRight(double power) {
+        frontRight.setPower(power*-1);
+        backRight.setPower(power);
+        frontLeft.setPower(power);
+        backLeft.setPower(power*-1);
+    }
+
+    public void backward(double power) {
+        frontRight.setPower(power*-1);
+        backRight.setPower(power*-1);
+        frontLeft.setPower(power*-1);
+        backLeft.setPower(power*-1);
+    }
+
+    public void forward(double power) {
+        frontRight.setPower(power);
+        backRight.setPower(power);
+        frontLeft.setPower(power);
+        backLeft.setPower(power);
+    }
+
+    public void spinLeft(double power) {
+        frontRight.setPower(power*-1);
+        backRight.setPower(power*-1);
+        frontLeft.setPower(power);
+        backLeft.setPower(power);
+    }
+
+    public void spinRight(double power) {
+        frontRight.setPower(power);
+        backRight.setPower(power);
+        frontLeft.setPower(power*-1);
+        backLeft.setPower(power*-1);
+    }
+
+    public void forward() {
+        forward(1);
+    }
+
+    public void strafeLeft() {
+        strafeLeft(1);
+    }
+
+    public void strafeRight() {
+        strafeRight(1);
+    }
+
+    public void backward() {
+        backward(1);
+    }
+
+    public void spinLeft() {
+        spinLeft(1);
+    }
+
+    public void spinRight() {
+        spinRight(1);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susannah/bob.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/susannah/bob.java
@@ -6,22 +6,12 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class bob extends LinearOpMode {
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
+
+    public Drive drive;
 
     @Override
     public void runOpMode() {
-        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
-        frontRight = hardwareMap.get(DcMotor.class, "front_right");
-        backLeft = hardwareMap.get(DcMotor.class, "back_left");
-        backRight = hardwareMap.get(DcMotor.class, "back_right");
-
-        frontLeft.setDirection(DcMotor.Direction.REVERSE);
-        backLeft.setDirection(DcMotor.Direction.REVERSE);
-        frontRight.setDirection(DcMotor.Direction.FORWARD);
-        backRight.setDirection(DcMotor.Direction.FORWARD);
+        drive = new Drive(hardwareMap);
 
         telemetry.addData("Initialized", "Ready to start");
         waitForStart();
@@ -97,71 +87,5 @@ public class bob extends LinearOpMode {
 
             telemetry.update();
         }
-    }
-
-    public void strafeLeft(double power) {
-        frontRight.setPower(power);
-        backRight.setPower(power*-1);
-        frontLeft.setPower(power*-1);
-        backLeft.setPower(power);
-    }
-
-    public void strafeRight(double power) {
-        frontRight.setPower(power*-1);
-        backRight.setPower(power);
-        frontLeft.setPower(power);
-        backLeft.setPower(power*-1);
-    }
-
-    public void backward(double power) {
-        frontRight.setPower(power*-1);
-        backRight.setPower(power*-1);
-        frontLeft.setPower(power*-1);
-        backLeft.setPower(power*-1);
-    }
-
-    public void forward(double power) {
-        frontRight.setPower(power);
-        backRight.setPower(power);
-        frontLeft.setPower(power);
-        backLeft.setPower(power);
-    }
-
-    public void spinLeft(double power) {
-        frontRight.setPower(power*-1);
-        backRight.setPower(power*-1);
-        frontLeft.setPower(power);
-        backLeft.setPower(power);
-    }
-
-    public void spinRight(double power) {
-        frontRight.setPower(power);
-        backRight.setPower(power);
-        frontLeft.setPower(power*-1);
-        backLeft.setPower(power*-1);
-    }
-
-    public void forward() {
-        forward(1);
-    }
-
-    public void strafeLeft() {
-        strafeLeft(1);
-    }
-
-    public void strafeRight() {
-        strafeRight(1);
-    }
-
-    public void backward() {
-        backward(1);
-    }
-
-    public void spinLeft() {
-        spinLeft(1);
-    }
-
-    public void spinRight() {
-        spinRight(1);
     }
 }
