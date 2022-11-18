fefd64e2c7d7c48c323028d0fa8ab6617cf4e6ad
Ronan <spacechickenrobot@gmail.com>
2022-11-18 13:38:22 -0500

    Moved LastYear Code to "lasyyear" root folder, and added encoder code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drive.java
@@ -83,4 +83,106 @@ public class Drive {
     public void spinRightRaw() {
         spinRight(1);
     }
+
+    //encoder stuffs
+    public void Run_with_encoder() {
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    public void run_using_encoder() {
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void run_to_position() {
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void stop_and_reset_encoders() {
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void forward(double power, int ticks) {
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            forwardRaw(power);
+        }
+    }
+
+    public void backward(double power, int ticks) {
+        ticks = (ticks*-1);
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            backwardRaw(power);
+        }
+    }
+
+    public void strafeRight(double power, int ticks) {
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks*-1);
+        backLeft.setTargetPosition(ticks*-1);
+        backRight.setTargetPosition(ticks);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            strafeRightRaw(power);
+        }
+    }
+
+    public void strafeLeft(double power, int ticks) {
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks*-1);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks*-1);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            strafeLeftRaw(power);
+        }
+    }
+
+    public void spinLeft(double power, int ticks) {
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks);
+        frontRight.setTargetPosition(ticks*-1);
+        backLeft.setTargetPosition(ticks);
+        backRight.setTargetPosition(ticks*-1);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            spinLeftRaw(power);
+        }
+    }
+
+    public void spinRight(double power, int ticks) {
+        stop_and_reset_encoders();
+        frontLeft.setTargetPosition(ticks*-1);
+        frontRight.setTargetPosition(ticks);
+        backLeft.setTargetPosition(ticks*-1);
+        backRight.setTargetPosition(ticks);
+        run_to_position();
+        while (frontLeft.isBusy()) {
+            spinRightRaw(power);
+        }
+    }
 }
